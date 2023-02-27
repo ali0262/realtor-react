b85ff0e55a3af94c2d02e6b49d61b9515e5c624d
ali <alifront2233@gmail.com>
2023-02-27 17:16:50 +0330

    create Realtor with react and tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='app'>
+            <h1 className='text-sky-300 text-center text-3xl underline'>hello world</h1>
+        </div>
+    );
+}
+
+export default App;
